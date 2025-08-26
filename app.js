8384b172995e3280d2d014dd985d5f53852be896
lauren-lester24 <lauren.lester24@yahoo.com>
2025-08-26 18:13:18 -0500

    created party planner

---
+++ app.js
@@ -0,0 +1,10 @@
+const API_BASE =
+  "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2506-FTB-CT-WEB-PT/events";
+
+// State
+let state = {
+  events: [],
+  selectedEvent: null,
+  error: null,
+};
+
