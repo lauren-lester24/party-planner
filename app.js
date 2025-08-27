741ce0ddf92962b2fa3cabd375b0444d6dfd4baa
lauren-lester24 <lauren.lester24@yahoo.com>
2025-08-27 16:49:34 -0500

    finished party planner

--- app.js
+++ app.js
@@ -1,10 +1,84 @@
-const API_BASE =
-  "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2506-FTB-CT-WEB-PT/events";
+const BASE_URL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api"
+
+const COHORT = "2506-FTB-CT-WEB-PT"
+
+const API = `${BASE_URL}/${COHORT}`
+
+console.log(API);
+
 
 // State
-let state = {
-  events: [],
-  selectedEvent: null,
-  error: null,
-};
+let events = [];
 
+
+// GET ALL THE RECIEPES
+async function getEvents(){
+try {
+  const response = await fetch(`${API}/events`);
+  const events = await response.json();
+
+  return events.data;
+render();
+
+} catch (error) {
+  console.error("There was an error /GET Event", error);
+}
+}
+
+// AWAIT response.JSON - turns body into javascript object
+async function getSingleEvent() {
+  try {
+    const response = await fetch(`${API}/events/${id}`)
+    const singleEvent = await response.json();
+console.log(singleEvent);
+    return singleEvent.data;
+
+
+  } catch (error) {
+    console.error("There was an error /GET Event", error);
+  }
+}
+
+
+
+
+
+
+function EventCard(event){
+
+const $card = document.createElement("article");
+$card.classList.add("event");
+
+$card.innerHTML = `
+ <h2>${event.name}</h2>
+            <figure>
+                <img src=${event.imageUrl} alt={event.name}>
+            </figure>
+            <p>${event.description}</p>
+`;
+return $card;
+}
+
+
+function EventCollection() {
+  const $collection = document.createElement("article");
+  $collection.classList.add("events");
+  const $events = events.map(EventCard);
+  $collection.replaceChildren(...$events);
+  return $events;
+}
+
+function render(){
+  const $app = document.querySelector("#app");
+  $app.innerHTML = `
+  <h1>Events</h1>
+<EventCollection></EventCollection>
+  `;
+  $app.querySelector("EventCollection").replaceWith(EventCollection());
+}
+
+async function init() {
+  await getEvents();
+  render();
+}
+init();
